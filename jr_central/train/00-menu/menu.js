b803189fad66b568f3bd0749a84b8fef1958aef5
Esaku-e39 <keih131010@gmail.com>
2023-12-09 08:09:02 +0900

    convert wav files to mp3

--- jr_central/train/00-menu/menu.js
+++ jr_central/train/00-menu/menu.js
@@ -1,6 +1,6 @@
 $(function () {
     // 変更すべき箇所(この3点は全て同じに！)
-    // 1. audio タグの src="../00-menu/sound/○○.wav" の部分のpath名
+    // 1. audio タグの src="../00-menu/sound/○○.mp3" の部分のpath名
     // 2. audio タグの id
     // 3. button タグの value
     $(".menu").html(
@@ -9,18 +9,18 @@ $(function () {
 
         '<h3>戸閉予告</h3>'+  /////////////////////////////////////////////////////////////////////////////
 
-        '<audio src="../00-menu/sound/doorClose_man.wav" id="doorClose_man" preload="none"></audio>' +
+        '<audio src="../00-menu/sound/doorClose_man.mp3" id="doorClose_man" preload="none"></audio>' +
         '扉閉予告 [日] <button class="play" value="doorClose_man">a</button><br>' +
 
 
         '<h3>雑案内</h3>'+ ////////////////////////////////////////////////////////////////////////////////////
 
-        '<audio src="../00-menu/sound/departing_soon.wav" id="departing_soon" preload="none"></audio>' +
+        '<audio src="../00-menu/sound/departing_soon.mp3" id="departing_soon" preload="none"></audio>' +
         '「まもなく発車します」[日英] <button class="play" value="departing_soon">a</button><br>' +
 
         // '<h3>マナー啓発</h3>'+ ///////////////////////////////////////////////////////////////////////////////
 
-        // '<audio src="../00-menu/sound/mobile_phone.wav" id="mobile_phone" preload="none"></audio>' +
+        // '<audio src="../00-menu/sound/mobile_phone.mp3" id="mobile_phone" preload="none"></audio>' +
         // '携帯マナーモード [日英] <button class="play" value="mobile_phone">a</button><br>' +
 
         '</details>'
